3352ada79760df4683184cac73ecc897b8cd5222
carrynong2 <carrynong2@gmail.com>
2023-12-23 20:46:17 +0700

    add router

---
+++ src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+</script>
+
+<template>
+  <div>
+    Hello Home
+    <RouterLink :to="{name: 'profile-edit', params: {id: 1}}">
+      <button>
+       Home
+      </button>
+    </RouterLink>
+  </div>
+</template>
---
+++ src/views/ProfileView.vue
@@ -0,0 +1,23 @@
+<script setup>
+  import { RouterLink,useRoute,useRouter } from 'vue-router';
+  const route = useRoute()
+  const router = useRouter()
+  const backToHome = () => {
+    router.push({name: "home"})
+  }
+</script>
+
+<template>
+  <div>
+    Profile Vue
+    <div>
+      Firstname <input type="text">
+    </div>
+    <div>
+      Lastname <input type="text">
+    </div>
+    <div>
+        <button @click="backToHome()">Back to home</button>
+    </div>
+  </div>
+</template>
